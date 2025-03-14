45007f18a99128712a05f2987cdfada0f8ca85d0
Angela Iruzubi <angelairuzubi97@gmail.com>
2025-03-14 09:50:30 +0100

    implementacion test equals()

--- src/test/java/com/mycompany/instituto/PersonaTest.java
+++ src/test/java/com/mycompany/instituto/PersonaTest.java
@@ -50,4 +50,20 @@ public class PersonaTest {
         assertEquals(20, persona.getEdad(), "La edad calculada no es correcta");
     }
     
+    @Test
+    public void testEquals() {
+        // Crear dos personas con el mismo NIF
+        Persona persona1 = new Persona(12345678, "Juan", 'H', 1, 1, 2000);
+        Persona persona2 = new Persona(12345678, "Ana", 'M', 2, 2, 2001);
+
+        // Verificar que las personas son iguales (mismo NIF)
+        assertTrue(persona1.equals(persona2), "Las personas con el mismo NIF deben ser iguales");
+
+        // Crear una tercera persona con un NIF diferente
+        Persona persona3 = new Persona(87654321, "Carlos", 'H', 3, 3, 2002);
+
+        // Verificar que las personas no son iguales (NIF diferente)
+        assertFalse(persona1.equals(persona3), "Las personas con NIF diferente no deben ser iguales");
+    }
+    
 }
