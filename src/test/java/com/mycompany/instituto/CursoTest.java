3bfc4e7d413824439f9e239e9fcccdc3432cf4e0
Angela Iruzubi <angelairuzubi97@gmail.com>
2025-03-14 09:46:47 +0100

    implementacion test aniadirAlumno

---
+++ src/test/java/com/mycompany/instituto/CursoTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.instituto;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author PC
+ */
+public class CursoTest {
+    
+    public CursoTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+
+    @Test
+    public void testAniadirAlumno() {
+       Curso curso = new Curso("Matemáticas");
+        Persona alumno = new Persona(12345678, "Juan", 'H', 1, 1, 2000);
+        
+        curso.aniadirAlumno(alumno);
+        assertTrue(curso.toString().contains("Juan"), "El alumno no fue añadido correctamente");
+    }
+    
+}
